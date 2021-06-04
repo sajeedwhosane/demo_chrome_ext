dd1c45872a2c308513d352f7bdfdc623b51b530a
sajeedwhosane <shaik.sajeed.16@gmail.com>
2021-06-04 19:46:36 +0530

    Extension which personalizes the new tab on chrome browser
    When a user opens a new tab,will ask for name and store it in local storage and will greet the user with the name

---
+++ newtab.js
@@ -0,0 +1,27 @@
+let gr=document.getElementById('greet');
+function greet(){
+    if(localStorage.getItem('Name')){
+        // if name is added in the local storage
+        let name=localStorage.getItem("Name");
+        gr.innerHTML =`<h1> Hello ${name} </h1>`;
+        console.log("storage item is set");
+    }else{
+        console.log("else loop");
+       let name = prompt("Please enter your name:","Name");
+        localStorage.setItem("Name",name);
+        greet();
+    }
+}
+
+window.addEventListener('load', greet());
+/*
+document.getElementById('submit').addEventListener( "click", getName);
+function getName(){
+    document.getElementsByTagName('body').innerHTML="";
+    let n= document.getElementById('name').value;
+    console.log("hey");
+    localStorage.setItem("Name",n);
+    let name=localStorage.getItem("Name");
+    console.log(name);
+}
+*/
